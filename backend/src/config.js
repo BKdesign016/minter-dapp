bb381d5141a268f0c817523e1d94385c285d4467
BKdesign <bkrecord016@gmail.com>
2022-05-11 15:41:08 +0200

    no whitelisted

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const ROYALTY_SHARE = 750; // Percentage of the token price that goes to the roy
 const ROYALTY_ADDRESS = "0x8589Dc71c3045e57fe9729073B02A063E6b46b99"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = ["0xD5c02939A20B6E1b9b59A32E2c896522e06223fD","0x8589Dc71c3045e57fe9729073B02A063E6b46b99"]; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
